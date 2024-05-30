f78bbea0ab52e6c83fa8753f6fa4ad967973d8b2
Torubiri Walter <toruwalt1997@gmail.com>
2024-05-30 14:49:05 +0100

    Created Flask app

---
+++ dotask.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""My Taskkeeper"""
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    """The homepage"""
+    return "<p>Hello, World!</p>"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
